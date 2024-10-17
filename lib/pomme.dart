0662500d7728a3e199af5a815c136f505d617597
ValDev99 <99172923+ValDev99@users.noreply.github.com>
2024-10-17 11:11:57 +0200

    feat: add all code

--- lib/pomme.dart
+++ lib/pomme.dart
@@ -10,7 +10,7 @@ class Pomme extends StatefulWidget {
 
 class _PommeState extends State<Pomme> {
   int _counter = 0;
-  List<int> _fruits = []; //
+  List<int> _fruits = [];
 
   void _incrementCounter() {
     setState(() {
@@ -19,12 +19,81 @@ class _PommeState extends State<Pomme> {
     });
   }
 
+  bool _isPrime(int number) {
+    if (number < 2) return false;
+    for (int i = 2; i <= number ~/ 2; i++) {
+      if (number % i == 0) return false;
+    }
+    return true;
+  }
+
+  bool _isEven(int number) => number % 2 == 0;
+
+  String _getNumberType(int number) {
+    if (_isPrime(number)) return "Nombre premier";
+    return _isEven(number) ? "Nombre pair" : "Nombre impair";
+  }
+
+  Image _getImageForNumber(int number) {
+    if (_isPrime(number)) {
+      return Image.asset('images/ananas.png');
+    } else if (_isEven(number)) {
+      return Image.asset('images/poire.png');
+    } else {
+      return Image.asset('images/pomme.png');
+    }
+  }
+
+  Color _getColorForNumber(int number) {
+    return _isEven(number) ? Colors.indigo : Colors.cyan;
+  }
+
+  void _showAlertDialog(int number) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: _getColorForNumber(number),
+          title: Text(_getNumberType(number)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _getImageForNumber(number),
+              const SizedBox(height: 20),
+              Text(
+                'Valeur: $number',
+                style: const TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _fruits.remove(number);
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text('Supprimer', style: TextStyle(color: Colors.red)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fermer'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('$_counter : ${_getNumberType(_counter)}'),
         centerTitle: true,
       ),
       body: Center(
@@ -35,8 +104,16 @@ class _PommeState extends State<Pomme> {
               child: ListView.builder(
                 itemCount: _fruits.length,
                 itemBuilder: (context, index) {
+                  int currentItem = _fruits[index];
+
                   return ListTile(
-                    title: Text('Item ${_fruits[index]}'),
+                    leading: _getImageForNumber(currentItem),
+                    title: Text(
+                      '$currentItem',
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    tileColor: _getColorForNumber(currentItem),
+                    onTap: () => _showAlertDialog(currentItem),
                   );
                 },
               ),
@@ -46,6 +123,9 @@ class _PommeState extends State<Pomme> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        backgroundColor: _counter == 0
+            ? Colors.orange
+            : _getColorForNumber(_counter),
         tooltip: 'Add Item',
         child: const Icon(Icons.add),
       ),
